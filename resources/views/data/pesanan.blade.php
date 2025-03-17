e31022ab118785584c8336d2172cbbd00507b970
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-03-17 15:00:23 +0700

    edit pesanan

--- resources/views/data/pesanan.blade.php
+++ resources/views/data/pesanan.blade.php
@@ -45,7 +45,27 @@ active
                     <button type="submit" class="btn btn-primary">Pembuatan Kwitansi</button>
                     <br>
                     <br>
-                    <button type="submit" class="btn btn-primary">Kirim</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Kirim
+                    </button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Mengirim</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                            Apakah Anda Yakin Mengirim Pesanan Ini
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak Setuju</button>
+                            <button type="button" class="btn btn-primary">Setuju</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    
                 </form>
                 </div>
                 </div>
--- resources/views/data/tambah.blade.php
+++ resources/views/data/tambah.blade.php
@@ -72,7 +72,27 @@ active
                         <input type="texy" name="1hari" class="form-control" id="exampleInputPassword1">
                     </div>
                     
-                    <button type="submit" class="btn btn-primary">Tambah</button> 
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Tambah
+                    </button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Kamar</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                            Apakah Data Yang Anda Isi Sudah Sesuai
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak Sesuai</button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Sesuai</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+ 
                     
                 </form>
                 
