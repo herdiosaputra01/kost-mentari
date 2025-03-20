9f8391702084d7dd4fcab663deff0af5d0e71ffe
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-03-20 11:45:50 +0700

    membuat logo navbar & modifikasi branda

--- resources/views/data/beranda.blade.php
+++ resources/views/data/beranda.blade.php
@@ -6,6 +6,7 @@ beranda
 active
 @endsection
 @section('konten')
+<br>
     <section class="container mx-auto p-8">
         <h1 class="text-4xl font-bold">Temukan Hunian Nyaman di Kos-Kosan Kami</h1>
         <p class="text-gray-700 mt-2">Selamat Datang di Kost Pria Pondok Mentari! Temukan Kenyamanan Sejati Dalam Hunian Kami.</p>
@@ -16,14 +17,51 @@ active
         </div>
         
 
-        <div class="container text-center mt-3 bg-white">
+        <div class="container text-center mt-6 bg-white">
             <div class="row">
-                <div class="col-4">
-                <img src="{{asset('image/kost.jpg')}}" class="img-thumbnail img-fluid" alt="">
+                <div class="col-6">
+                    <img src="{{asset('image/kost.jpg')}}" class="img-thumbnail img-fluid" alt="kost">
                 </div>
+
+                        <div class="col-6">
+                            <title>Kamar</title>
+                            <style>
+                                .frame {
+                                    width: 350px; 
+                                    height: auto;
+                                    padding: 20px;
+                                    border-radius: 50px 50px 0 0;
+                                    display: flex;
+                                    justify-content: right;
+                                    align-items: right;
+                                    position: relative;
+                                }
+                                .frame img {
+                                    width: 100%;
+                                    height: auto;
+                                    border-radius: 50px 50px 0 0;
+                                }
+                                .border-frame {
+                                    position: absolute;
+                                    top: 40px;
+                                    left: 1px;
+                                    width: 90%;
+                                    height: 90%;
+                                    border: 3px solid black;
+                                    border-radius: 50px 50px 0 0; 
+                                }
+                            </style>
+                        
+                            <div class="frame">
+                                <img src="{{ asset('image/kamar.jpg') }}" alt="Kamar">
+                                <div class="border-frame"></div>
+                            </div>
+                        </div>
             </div>
+        </div>
 
     </section>
+    
     @endsection
 
 
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,6 +11,9 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-secondary navbar-secondary">
         <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('image/lg.png') }}" alt="Logo" width="50" height="50">
+            </a>
             <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
